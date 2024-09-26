f42e94b56b4f4bb645bc118e42520af02fe4af46
Jesus Cobo <146002181+JesusCoboArrogante@users.noreply.github.com>
2024-09-26 20:10:47 +0200

    first commit

---
+++ 1.Trimestre/ejercicios/ejercicio01/ejercici02/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val ancho: Int = 0
+    val largo: Int = 0
+
+    println("dame el ancho")
+    ancho=readln().toInt()
+}
---
+++ 1.Trimestre/ejercicios/ejercicio01/ejercicio04/src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var numeroUno:Double = 0.0
+    var numeroDos: Double = 0.0
+    var numeroTres: Double = 0.0
+
+
+
+    println("introduce un numero")
+    numeroUno = readln().toDouble()
+
+    println("introduce otro numero")
+    numeroDos = readln().toDouble()
+
+    println("introduce otro numero")
+    numeroTres = readln().toDouble()
+
+    var media : Double= (numeroUno + numeroDos + numeroTres) / 3
+
+    if (media )
+
+    println(media)
+
+
+
+}
